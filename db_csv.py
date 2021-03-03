7464df89016659fe2d57ce565a8353bc60448dce
jyothsnashaji <jyothsnashaji99@gmail.com>
2021-03-03 11:48:36 +0530

    Added support for accessing data from csv

--- db_csv.py
+++ db_csv.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import datetime
 
 def get_col(param,router_id):
     df=pd.read_csv('Routerdata.csv')
@@ -31,3 +31,44 @@ def get_router_id(row):
     df=df.sort_values('Router_id')
     temp=df['Router_id'].unique()
     return str(temp[row])
+
+
+def get_col_group(param):
+
+    if param == "network":
+        df = ["ipv4", "ipv6", "mpls", "mac"]
+    elif param == "software":
+        df = ["iosd", "res", "err"]
+    elif param == "hardware":
+        df = ["cpu", "mem", "fan", "power", "tcam", "faults"]
+
+    return df
+
+
+def compute_score(router_id, param):
+
+    now = datetime.datetime.now().replace(second=0, microsecond=0) - 3 * \
+        datetime.timedelta(minutes=1)  # time stamp of detected cpu usage
+
+    if (param == "summary"):
+        return max(compute_score(router_id, "network"), compute_score(router_id, "hardware"), compute_score(router_id, "software"))
+
+    # fix now
+    df = get_col(param, router_id)
+    print("db",df)
+
+    if any(float(x) > 95 for x in df):
+        score = 150
+    elif any(float(x) > 85 for x in df):
+        score = 90
+    else:
+        score = 30
+    return score
+
+
+def get_title(param):
+    titles = {'cpu': 'Total CPU Utilization', 'iosd': 'IOSd Process Utilization', 'mem': 'Total Memory Utilization',
+              'ipv4': 'IPv4 Route Utilization', 'ipv6': 'IPv6 Route Utilization', 'mac': 'MAC Table Utilization',
+              'fan': 'Fan Speed', 'power': 'Power', 'mpls': 'MPLS Label Utilization', 'tcam': 'External TCAM(KBP) Utilization',
+              'res': 'ID Allocation', 'err': 'Errors/Pending Objects', 'faults': 'Faults on the IM cards'}
+    return titles[param]
